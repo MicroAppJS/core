6cf123b5655f78d6b1e1a08916caafb0a908255b
zyao89 <zyao89@gmail.com>
2019-03-11 19:28:16 +0800

    :bug: fixed

--- utils/module-alias.js
+++ utils/module-alias.js
@@ -12,32 +12,44 @@ function injectAliasModule(names) {
     names.forEach(key => {
         if (!injectCache[key]) {
             const microConfig = requireMicro(key);
-            if (microConfig) {
-                const moduleAlias = tryRequire('module-alias');
-                if (moduleAlias) {
-                    // inject shared
-                    const alias = {};
-                    const aliasName = microConfig.name;
-                    if (aliasName) {
-                        const aliasKey = aliasName[0] !== '@' ? `@${aliasName}` : aliasName;
-                        if (aliasName) {
-                            Object.keys(microConfig.shared).forEach(k => {
-                                const p = microConfig.shared[k];
-                                if (p && typeof p === 'string' && !alias[`${aliasKey}/${k}`]) {
-                                    const filePath = path.resolve(microConfig.root, p);
-                                    alias[`${aliasKey}/${k}`] = filePath;
-                                }
-                            });
-                        }
-                    }
-                    if (alias && JSON.stringify(alias) !== '{}') {
-                        moduleAlias.addAliases(alias);
-                        injectCache[key] = true;
-                    }
-                }
-            }
+            injectSelfAliasModule(microConfig, key);
         }
     });
+
+    // inject self
+    const selfMicroConfig = requireMicro.self();
+    injectSelfAliasModule(selfMicroConfig, '__self__');
+}
+
+function injectSelfAliasModule(microConfig, key) {
+    if (injectCache[key]) {
+        return;
+    }
+    if (!microConfig) {
+        return;
+    }
+    const moduleAlias = tryRequire('module-alias');
+    if (moduleAlias) {
+        // inject shared
+        const alias = {};
+        const aliasName = microConfig.name;
+        if (aliasName) {
+            const aliasKey = aliasName[0] !== '@' ? `@${aliasName}` : aliasName;
+            if (aliasName) {
+                Object.keys(microConfig.shared).forEach(k => {
+                    const p = microConfig.shared[k];
+                    if (p && typeof p === 'string' && !alias[`${aliasKey}/${k}`]) {
+                        const filePath = path.resolve(microConfig.root, p);
+                        alias[`${aliasKey}/${k}`] = filePath;
+                    }
+                });
+            }
+        }
+        if (alias && JSON.stringify(alias) !== '{}') {
+            moduleAlias.addAliases(alias);
+            injectCache[key] = true;
+        }
+    }
 }
 
 module.exports = function(...names) {
