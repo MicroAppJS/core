c5686bc7155631d2d3db0756fa0158940d0f18ec
zyao89 <zyao89@gmail.com>
2019-07-17 18:37:49 +0800

    :add: 增加 vusion-core 适配 adapter

--- adapter/index.js
+++ adapter/index.js
@@ -6,5 +6,6 @@ adapters.CommonAdapter = require('./common');
 adapters.KoaAdapter = require('./koa');
 adapters.VusionAdapter = require('./vusion');
 adapters.WebpackAdapter = require('./webpack');
+adapters.VusionCoreAdapter = require('./vusion-core');
 
 module.exports = adapters;
